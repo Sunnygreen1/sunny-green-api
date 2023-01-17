5b45b0c6e811d8acd0dae6f3f0f6d4288fff6a75
Omnivers <Twersimor.stock@outlook.com>
2023-01-17 22:25:01 +0100

    cors test

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ require('dotenv').config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
 
 app.listen(process.env.PORT||3000, () => {
     console.log(`Le serveur écoute sur le port 3000`);
